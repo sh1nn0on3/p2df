7469aacc7a42e8061e5e1044c1c259bd4f126fde
sh1nn0on3 <sanogaku31@gmail.com>
2025-10-29 20:43:51 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    return redirect()->route('/');
+    return redirect('/');
 });
 // ===========================
 // Authentication Routes
